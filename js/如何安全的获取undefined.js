9a5ce82310c634b6d240a96aaa9c1916c6ca24fc
liujin <2454243839@qq.com>
2023-04-18 10:10:59 +0800

    1

--- js/如何安全的获取undefined.js
+++ js/如何安全的获取undefined.js
@@ -1,9 +1,7 @@
 const test = undefined;
 let other;
-if (typeof test === "undefined") {
-  console.log("true");
-}
 
+console.log(typeof test === "undefined");
 console.log(other === undefined);
 console.log(test === undefined);
 console.log(test === void 0);

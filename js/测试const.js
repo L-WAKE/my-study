a38c1d01f06a04dbd48e04cf214c918fe4438c63
liujin <2454243839@qq.com>
2023-05-10 16:37:19 +0800

    entries()

---
+++ js/测试const.js
@@ -0,0 +1,11 @@
+// const 不能直接将一个变量赋值给已定义的变量，
+// 可以修改里面的属性或者添加新的属性，因为他们指向的内存地址还是同一个地址
+
+const obj = { key: "测试的", val: "vvval" };
+const newObj = { name: "名字", age: 18 };
+
+// obj = newObj;
+
+obj.key = "555";
+newObj.sex = "男";
+console.log(obj, newObj);

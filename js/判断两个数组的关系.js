9d30d19cf73adf1e2012ff96850ea5ecaead5d07
liujin <2454243839@qq.com>
2023-06-06 16:33:57 +0800

    数组关系

---
+++ js/判断两个数组的关系.js
@@ -0,0 +1,25 @@
+// 判断两个数组是否完全相等
+const scalarArrayEquals = (array1, array2) => {
+  return (
+    array1.length == array2.length &&
+    array1.every(function (v, i) {
+      return v === array2[i];
+    })
+  );
+};
+const sarr1 = ["t1"];
+const sarr2 = ["t1", 22];
+console.log("scalarArrayEquals", scalarArrayEquals(sarr1, sarr2));
+
+// 判断一个数组是否包含另一个数组
+const getInclude = (arr1, arr2) => {
+  let temp = [];
+  let notTmp = [];
+  for (const item of arr1) {
+    arr2.find((i) => i === item) ? temp.push(item) : notTmp.push(item);
+  }
+  return notTmp.length > 0 ? false : true;
+};
+const garr1 = ["2", "3"];
+const garr2 = ["3"];
+console.log("getInclude", getInclude(garr1, garr2));

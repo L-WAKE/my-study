cd8bcae2e194b8c3f32083f3fe514332abcbc568
liujin <2454243839@qq.com>
2023-06-07 10:50:26 +0800

    every方法的使用

--- js/判断两个数组的关系.js
+++ js/判断两个数组的关系.js
@@ -35,3 +35,15 @@ const findElement = (arr1, arr2) => {
 const farr1 = ["2", "3", "44"];
 const farr2 = ["44"];
 console.log("findElement", findElement(farr1, farr2));
+
+/*
+  every方法的使用
+  every()方法用于检测数组中的所有元素是否都满足指定条件（该条件为一个函数）。
+  every()方法会遍历数组的每一项，如果有有一项不满足条件，则表达式返回false,
+  剩余的项将不会再执行检测；如果遍历完数组后，每一项都符合条，则返回true。
+*/
+const earr = [200, 120, 600, 50];
+const flag = earr.every(function (v, i) {
+  return v > 20;
+});
+console.log("flag", flag);

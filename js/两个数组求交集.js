b62390f24046f0e3a24612f364424720eb58d0a9
liujin <2454243839@qq.com>
2023-06-09 11:18:19 +0800

    删除数组中的NAN

--- js/两个数组求交集.js
+++ js/两个数组求交集.js
@@ -17,3 +17,16 @@ const beMixed = (arr = [], arr2 = []) => {
 
 const mixedArr = beMixed(array, arrayOther);
 console.log(mixedArr);
+
+const deleArr = [5, 6, 4, NaN, 1, NaN];
+//删除数组中的NAN
+function deleteNaN(arr) {
+  let resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i]) {
+      resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
+}
+console.log("deleteNaN", deleteNaN(deleArr));

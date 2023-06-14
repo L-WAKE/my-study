978142c72a74aeafe0c721780c475c397dad1390
liujin <2454243839@qq.com>
2023-06-14 10:07:21 +0800

    动态规划法

--- js/最长递增子序列.js
+++ js/最长递增子序列.js
@@ -63,7 +63,7 @@ function longestIncreasingSubsequenced(arr) {
     }
   }
   return Math.max(...dp);
-}
+} 
 const numsd = [10, 9, 2, 5, 3, 7, 101, 18];
 const subsequenced = longestIncreasingSubsequenced(numsd);
 console.log(subsequenced); // 输出: 4

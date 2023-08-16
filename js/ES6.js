7e55028ec081f7c860f46ff1c7f33c8ce48a9d3d
liujin <2454243839@qq.com>
2023-08-16 09:58:53 +0800

    属性的遍历

--- js/ES6.js
+++ js/ES6.js
@@ -1,5 +1,20 @@
-// Object.is()
+//1. Object.is()
 console.log(+0 === -0); //true
 console.log(Object.is(+0, -0)); // false
 console.log(NaN === NaN); // false
 console.log(Object.is(NaN, NaN)); // true
+
+//2. 属性的遍历
+/*
+    首先遍历所有数值键，按照数值升序排列
+    其次遍历所有字符串键，按照加入时间升序排列
+    最后遍历所有 Symbol 键，按照加入时间升序排
+*/
+let obj = { [Symbol()]: 0, b: 0, 10: 0, 2: 0, a: 0 };
+for (let key in obj) {
+  console.log("for..in", key);
+}
+console.log("Object.keys", Object.keys(obj));
+console.log("Object.getOwnPropertyNames", Object.getOwnPropertyNames(obj));
+console.log("Object.getOwnPropertySymbols", Object.getOwnPropertySymbols(obj));
+console.log("Reflect.ownKeys", Reflect.ownKeys(obj));

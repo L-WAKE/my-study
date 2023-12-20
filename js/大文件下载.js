c82ca1dcae3dd653438a6be829a7746ca4030395
liujin <2454243839@qq.com>
2023-12-20 13:37:05 +0800

    大文件拉取

--- js/大文件下载.js
+++ js/大文件下载.js
@@ -3,4 +3,14 @@ async function loadData() {
     "https://lf3-csp.bytetos.com/obj/ies-csp-resource/ies/helpcenter_web/common/vendor.76e67201.js";
   const response = await fetch(url);
   const reader = response.body.getReader();
+  let utfBdecoder = new TextDecoder();
+  for (;;) {
+    const { value, done } = await reader.read();
+    if (done) {
+      break;
+    }
+    const text = utfBdecoder.decode(value);
+    console.log(text);
+  }
 }
+loadData();
